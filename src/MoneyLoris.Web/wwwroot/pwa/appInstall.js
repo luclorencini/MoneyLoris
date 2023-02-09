d5ebd2b768075df8e6ae2e59a4c9a9e984b94a3f
Luciano Lorencini <luciano.lorencini@prodest.es.gov.br>
2023-02-09 15:38:37 -0300

    pwa - rearranjo arquivos / link para instalação do app

---
+++ src/MoneyLoris.Web/wwwroot/pwa/appInstall.js
@@ -0,0 +1,44 @@
+﻿const appInstall = {
+
+    instalar() {
+
+        let appInstall = document.querySelector('#pwaAppInstaller');
+
+        let deferredPrompt;
+     
+        window.addEventListener('beforeinstallprompt', e => {
+            e.preventDefault();
+            deferredPrompt = e;
+
+            if (this.showAppHint()) {
+                appInstall.classList.remove('d-none');
+            }
+        });
+      
+        appInstall.addEventListener('click', async (e) => {
+
+            if (this.showAppHint()) {
+
+                deferredPrompt.prompt();
+                await deferredPrompt.userChoice;
+
+                deferredPrompt = null;
+            }
+        });
+      
+        window.addEventListener('appinstalled', e => {
+            appInstall.classList.add('d-none');
+            alert("App instalado com sucesso na tela inicial!");
+        });
+
+    },
+
+    showAppHint() {
+        //só mostra prompt de instalação se não veio do link do pwa
+        const urlParams = new URLSearchParams(window.location.search);
+        const source = urlParams.get('source');
+        return (!source || source != 'pwa');
+    }
+}
+
+appInstall.instalar();
