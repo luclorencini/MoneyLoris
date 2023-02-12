63bed64a58057847667bcb4721f4074e56aa3a63
Luciano Lorencini <lorencini+pratica@gmail.com>
2023-02-12 00:49:46 -0300

    web - categoria - listagem

---
+++ src/MoneyLoris.Web/Controllers/CategoriaController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoneyLoris.Web.Controllers.Base;
+
+namespace MoneyLoris.Web.Controllers;
+public class CategoriaController : BaseController
+{
+    public IActionResult Index()
+    {
+        return View("CategoriaIndex");
+    }
+}
