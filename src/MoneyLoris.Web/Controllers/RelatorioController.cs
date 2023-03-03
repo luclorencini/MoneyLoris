3a554ce1e6a63fe423db143fa3e54f7b8997f838
Luciano Lorencini <luciano.lorencini@prodest.es.gov.br>
2023-03-03 17:17:42 -0300

    report - inicio

---
+++ src/MoneyLoris.Web/Controllers/RelatorioController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoneyLoris.Web.Controllers.Base;
+
+namespace MoneyLoris.Web.Controllers;
+
+[Route("[controller]/{action=index}")]
+public class RelatorioController : BaseController
+{
+    public IActionResult Index()
+    {
+        return View("~/Views/Relatorio/LancamentoCategoria/LancamentoCategoriaIndex.cshtml");
+    }
+}
