a182e53cdd59bf0fa7f885c0e147b67f5f1162b9
Luciano Lorencini <lorencini+pratica@gmail.com>
2023-11-02 15:43:47 -0300

    front-end - pagamento fatura

--- src/MoneyLoris.Application/Business/Lancamentos/Dtos/TransferenciaInsertDto.cs
+++ src/MoneyLoris.Application/Business/Lancamentos/Dtos/TransferenciaInsertDto.cs
@@ -5,4 +5,8 @@ public class TransferenciaInsertDto
     public int IdMeioPagamentoOrigem { get; set; }
     public int IdMeioPagamentoDestino { get; set; }
     public decimal Valor { get; set; }
+
+    //campos para cartão de crédito
+    public int? FaturaMes { get; set; }
+    public int? FaturaAno { get; set; }
 }
